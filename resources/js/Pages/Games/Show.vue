debfea6e79f6db6d18ddc732badc2216b5d42905
Pedro Borges <oi@pedroborg.es>
2024-08-14 15:17:23 -0300

    Add useGameState composable

--- resources/js/Pages/Games/Show.vue
+++ resources/js/Pages/Games/Show.vue
@@ -2,16 +2,20 @@
 import { computed, ref } from 'vue';
 import { Head } from '@inertiajs/vue3';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import Modal from '@/Components/Modal.vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+import { useGameState, gameStates } from "@/Composables/useGameState.js";
 
 defineProps({
     game: Object,
 });
 
 const boardState = ref([0, 0, 0, 0, 0, 0, 0, 0, 0])
+const gameState = useGameState();
 
 const xTurn = computed(() => boardState.value.reduce((carry, value) => carry + value, 0) === 0)
 
-const winningLines = [
+const lines = [
     // rows
     [0, 1, 2],
     [3, 4, 5],
@@ -28,21 +32,32 @@ const winningLines = [
 function fillSquare(index) {
     boardState.value[index] = xTurn.value ? -1 : 1
 
-    const winningLine = winningLines
-        .map((lines) => lines.reduce((carry, index) => carry + boardState.value[index], 0))
+    checkForVictory()
+}
+
+function checkForVictory() {
+    const winningLine = lines
+        .map((line) => line.reduce((carry, index) => carry + boardState.value[index], 0))
         .find((sum) => Math.abs(sum) === 3)
 
     if (winningLine === -3) {
-        return alert('X has won!')
+        gameState.change(gameStates.XWins)
+        return
     }
 
     if (winningLine === 3) {
-        return alert('O has won!')
+        gameState.change(gameStates.OWins)
+        return
     }
 
     if (! boardState.value.includes(0)) {
-        alert('Stalemate!')
+        gameState.change(gameStates.Stalemate)
     }
+}
+
+function resetGame() {
+    boardState.value = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    gameState.change(gameStates.InProgress)
 }
 </script>
 
@@ -56,5 +71,18 @@ function fillSquare(index) {
                 <span v-else v-text="square === -1 ? 'X' : 'O'" class="text-4xl font-medium"></span>
             </li>
         </ul>
+        <Modal @close="resetGame()" :show="gameState.hasEnded()">
+            <div class="p-6">
+                <div class="text-6xl font-bold text-center my-12 font-mono uppercase">
+                    <span v-if="gameState.current() === gameStates.XWins" class="text-green-600">X has won!</span>
+                    <span v-else-if="gameState.current() === gameStates.OWins" class="text-green-600">O has won!</span>
+                    <span v-else class="text-orange-600">Stalemate!</span>
+                </div>
+
+                <div class="mt-6 flex justify-end">
+                    <PrimaryButton @click="resetGame()">Play again</PrimaryButton>
+                </div>
+            </div>
+        </Modal>
     </AuthenticatedLayout>
 </template>
